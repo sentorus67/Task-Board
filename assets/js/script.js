c0581af1ecb82c9ad5d7972daff61a96b86d0eb1
Jemeric <tyuity14450@hotmail.com>
2024-03-31 15:03:37 -0400

    Added delete functionality to added task bodies

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,31 +20,35 @@ function generateTaskId() {
 }
 
 // Todo: create a function to create a task card
-function createTaskCard(task) {
+function createTaskCard(title, description, date) {
    const cardBody= document.createElement("body");
    const cardHeader= document.createElement('header');
    const cardMain= document.createElement('main');
    const cardFooter= document.createElement('button');
 
-   const cardHeadText= document.createElement('h2');
+   const cardHeadText= document.createElement('h3');
    const cardBodyText1= document.createElement('p');
    const cardBodyText2= document.createElement('p');
-   const cardFooterText= document.createElement('h2');
+   const cardFooterText= document.createElement('h3');
 
-   cardHeadText.textContent=generateTaskId();
-   cardBodyText1.textContent=generateTaskId();
-   cardBodyText2.textContent=generateTaskId();
+   cardHeadText.textContent=title;
+   cardBodyText1.textContent=description;
+   cardBodyText2.textContent=date;
    cardFooterText.textContent="Remove";
 
    $(cardBody).addClass('body');
    $(cardHeader).addClass('header');
    $(cardMain).addClass('main');
-   $(cardFooter).addClass('footer');
+   $(cardFooter).addClass('footer delete-btn');
 
    cardHeader.appendChild(cardHeadText);
    cardMain.appendChild(cardBodyText1);
    cardMain.appendChild(cardBodyText2);
    cardFooter.appendChild(cardFooterText);
+
+   cardFooter.addEventListener('click',function(){
+    handleDeleteTask(cardFooter)
+   });
 
    cardBody.appendChild(cardHeader);
    cardBody.appendChild(cardMain);
@@ -61,12 +65,18 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+    
+    const cardTitle=$('#title').val();
+    const cardDescription=$('#Due-Date').val();
+    const cardDate=$('#Description').val();
+    createTaskCard(cardTitle,cardDescription,cardDate);
 
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
-
+function handleDeleteTask(x){
+   const parent=x.parentNode;
+   parent.remove();
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
@@ -74,18 +84,25 @@ function handleDrop(event, ui) {
 
 }
 
-$("#openTaskForm").on("click", function() 
-  {
-    $( "#enterTask" ).dialog();
-  } );
-
-
-$("#submitButton").on("click",function(){
-    createTaskCard();
-});
-
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    
+    $("#openTaskForm").on("click", function() 
+    {
+      $( "#enterTask" ).dialog();
+    });
+  
+  
+  $("#submitButton").on("click",function(){
+     if($('#title').val() !='' && $('#Due-Date').val()!='' && $('#Description').val() !='' )
+      {
+      handleAddTask();
+      $( "#enterTask" ).dialog("close");
+      }
+      else
+      {
+          alert('One or more boxes are empty.')
+      }
+  });
 });
